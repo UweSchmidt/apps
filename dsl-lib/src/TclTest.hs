5e84f9504194ee88a31284e05370582b9193884b
Emil Steinberger <emil@delle.fh-wedel.de>
2011-06-29 15:50:54 +0200

    Experiments within TclTest

--- dsl-lib/src/TclTest.hs
+++ dsl-lib/src/TclTest.hs
@@ -1,7 +1,13 @@
+{-# OPTIONS -XTypeSynonymInstances  -XFlexibleContexts -XFlexibleInstances -XMultiParamTypeClasses #-}
+
 module TclTest
 where
 
+import Control.Arrow (first, second, (&&&), (***))
+import Control.Applicative ((<$>))
+import Control.Monad.Identity
 import Control.Monad.Error
+import Control.Monad.State
 import Language.Common.Eval
 
 import Language.Tcl.AbstractSyntax
@@ -10,7 +16,7 @@ import Language.Tcl.Show
 import Language.Tcl.Eval
 import Language.Tcl.Value
 
-import Text.Parsec
+-- import Text.Parsec
 
 -- ------------------------------------------------------------
 
@@ -29,4 +35,59 @@ testTcl :: Error err => Eval err (TclEnv ()) wrt (TclState e ()) res -> IO (Eith
 testTcl s
     = runEval s  (initTclEnv ()) (initTclState ())
 
+-- ------------------------------------------------------------
+{-
+type EvalOption os v m = StateT (os, [v]) m
+
+type TclOption os e s = EvalOption os Value (TclEval e s)
+
+type TestOption = EvalOption [String] String Maybe
+
+-- evalOptions :: os -> [v] -> EvalOption os v m a -> m (a, (os, [v]))
+-- evalOptions :: t -> t1 -> State (t, t1) a -> (a, (t, t1))
+-- evalOptions :: t -> t1 -> StateT (t, t1) m a -> m (a, (t, t1))
+-- evalOptions :: Monad m => t -> t1 -> StateT (t, t1) m a -> m (t, t1)
+
+evalOptions :: (Monad m) => os -> [v] -> EvalOption os v m a -> m (os, [v])
+evalOptions os vs eval = execStateT eval (os, vs)
+
+arg0 :: (Functor m, Monad m, MonadPlus m, MonadState (os, [v]) m) => EvalOption os v m v
+arg0 = do vs <- snd <$> get
+          if null vs
+             then mzero
+             else return (head vs)
+
+modifyOS :: (Monad m) => (os -> os) -> EvalOption os v m ()
+modifyOS osf
+    = modify (first osf)
+
+evalOpt0 :: (Functor m, Monad m, MonadPlus m, MonadState (os, [v]) m) =>
+            (v -> Bool) -> (os -> os) -> EvalOption os v m ()
+evalOpt0 name meaning
+    = do a <- arg0
+         if name a
+            then modify (meaning *** tail)
+            else return ()
+
+-- instance MonadState ([String], [String]) Maybe
+
+t1 :: Maybe ([String],[String])
+t1 = evalOptions [] ["-abc"] (evalOpt0 (== "-abc") ("abc":))
+
+ 
+-- instance (MonadPlus m) => MonadPlus (EvalOption s o m)
+
+-- runEvalOption :: EvalO
+  -}
+{-
+tclOpt1 :: (MonadPlus m) => Values -> m (a, Values)
+tclOpt1 f vs
+    = case f
+
+n v (x : xs)
+    | n == selS x
+	= return (v, xs)
+    | otherwise
+        = mzero
+-}
 -- ------------------------------------------------------------
