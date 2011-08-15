3ff9ca2b8a72fad8bd0ad8ee90d146940e60fda5
Theo Lingen <theo@meissel.(none)>
2011-08-15 18:34:29 +0200

    in tcl array command extended

--- dsl-lib/src/Language/Tcl/Commands/Array.hs
+++ dsl-lib/src/Language/Tcl/Commands/Array.hs
@@ -7,7 +7,7 @@ import Control.Applicative              ( (<$>) )
 
 import Control.Monad.RWS                ( mplus )
 
--- import Data.List                        ( intercalate )
+import Data.List                        ( intercalate )
 import qualified
        Data.Map                         as M
 import Language.Tcl.CheckArgs
@@ -20,41 +20,56 @@ import Language.Tcl.Value
 tclArray :: TclCommand e s
 tclArray (cmd' : name' : args)
     | cmd == "exists"
-        = arrayExists name args
+        = arrayExists an args
     | cmd == "get"
-        = arrayGet name args
+        = arrayGet    an args'
+    | cmd == "names"
+        = arrayNames  an args'
     | cmd == "set"
-        = arraySet name args
+        = arraySet    an args
     | cmd == "size"
-        = arraySize name args
+        = arraySize   an args
+    | cmd == "unset"
+        = arrayUnset  an args'
     | otherwise
         = tclThrowError
           $ "array option must be one of "
-                ++ unwords (map show ["exists", "get", "set", "size"])
+                ++ ( intercalate ", " . map show
+                     $ ["exists", "get", "set", "size"]
+                   )
     where
-      cmd  = selS cmd'
-      name = selS name'
+      cmd   = selS cmd'
+      name  = selS name'
+      an    = (name, Nothing)
+      args' = map selS args
 
 tclArray _
     = tclWrongArgs "array option arrayName ?arg arg ...?"
 
-arrayExists :: String -> TclCommand e s
+-- ------------------------------------------------------------
+
+arrayExists :: TclVarName -> TclCommand e s
 arrayExists an []
-    = b2i <$> varName isAVN (an, Nothing)
+    = b2i <$> varName isAVN an
 
 arrayExists _ _
     = tclWrongArgs "array exists arrayName"
 
-arrayGet :: String -> TclCommand e s
+-- ------------------------------------------------------------
+
+arrayGet :: TclVarName -> [String] -> TclEval e s Value
 arrayGet an []
-    = arrayGet an [mkS "*"]
+    = arrayGet an ["*"]
 
 arrayGet an [p]
-    = ( lookupArrayVar (an, Nothing)
+    = ( lookupArrayVar an
         >>= return
                 . mkL
                 . concatMap (\ (k, v) -> [mkS k, v])
-                . filter (matchGlobPattern (selS p) . fst)
+                . ( if p == "*"
+                    then id
+                    else filter (matchGlobPattern p . fst)
+                  )
                 . M.toList
       )
       `mplus`
@@ -63,25 +78,61 @@ arrayGet an [p]
 arrayGet _ _
     = tclWrongArgs "array get arrayName ?pattern?"
 
-arraySet :: String -> TclCommand e s
+-- ------------------------------------------------------------
+
+arrayNames :: TclVarName -> [String] -> TclEval e s Value
+arrayNames an []
+    = arrayNames' id an
+
+arrayNames an (m : p : [])
+    | m == "-exact"
+        = arrayNames' (filter (== p)) an
+    | m == "-glob"
+        = arrayNames' (filter (matchGlobPattern p)) an
+    | otherwise
+        = tclThrowError $ "bad option " ++ show m ++ ": must be -exact or -glob"
+
+arrayNames an [p]
+    = arrayNames' (filter (matchGlobPattern p)) an
+
+arrayNames _ _
+    = tclWrongArgs "array get arrayName ?mode? ?pattern?"
+
+arrayNames' :: ([String] -> [String]) -> TclVarName -> TclEval e s Value
+arrayNames' filter' an
+    = ( lookupArrayVar an
+        >>= return
+                . mkL
+                . map mkS
+                . filter'
+                . M.keys
+      )
+      `mplus`
+      return lempty
+
+-- ------------------------------------------------------------
+
+arraySet :: TclVarName -> TclCommand e s
 arraySet an [l]
     = do vs <- checkListValue l
-         createArray (an, Nothing)
+         createArray an
          setVals vs >> return mempty
     where
       setVals []
           = return ()
       setVals (i : v : r)
-          = setVar (an, Just . selS $ i) v >> setVals r
+          = setVar (fst an, Just . selS $ i) v >> setVals r
       setVals _
           = tclThrowError "list must have an even number of elements"
 
 arraySet _ _
     = tclWrongArgs "array set arrayName list"
 
-arraySize :: String -> TclCommand e s
+-- ------------------------------------------------------------
+
+arraySize :: TclVarName -> TclCommand e s
 arraySize an []
-    = ( lookupArrayVar (an, Nothing)
+    = ( lookupArrayVar an
         >>= return . mkI . toInteger . M.size
       )
       `mplus`
@@ -89,5 +140,25 @@ arraySize an []
 
 arraySize _ _
     = tclWrongArgs "array size arrayName"
+
+-- ------------------------------------------------------------
+
+arrayUnset :: TclVarName -> [String] -> TclEval e s Value
+arrayUnset an [p]
+    = lookupArrayVar an
+      >>= return    . filter (matchGlobPattern p) . M.keys
+      >>= sequence_ . map (\ i -> unsetVar (fst an, Just i))
+      >>  return mempty
+
+arrayUnset vn []
+    = ( lookupArrayVar vn	-- check whether an denotes an array
+        >> unsetVar vn          -- remove the whole array
+        >> return mempty
+      )
+      `mplus`
+      return mempty
+
+arrayUnset _ _
+    = tclWrongArgs "array unset arrayName ?pattern?"
 
 -- ------------------------------------------------------------
