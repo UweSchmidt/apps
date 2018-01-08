e787d50ed9504852006c32d9f191ff204287d84a
Uwe Schmidt <uwe@fh-wedel.de>
2018-01-08 12:44:48 +0100

    part2 of day13 solved

--- toys/adventofcode.com/Year17/Day10/Main.hs
+++ toys/adventofcode.com/Year17/Day10/Main.hs
@@ -4,123 +4,15 @@
 module Main where
 
 import Util.Main1    (main12)
-import Data.List     (foldl', unfoldr)
-import Data.Bits     (xor)
-import Text.Printf   (printf)
+
+-- TyingTheKnot is used in Year17.Day14
+import Year17.Day10.TyingTheKnot
 
 -- ----------------------------------------
 
 main :: IO ()
-main = main12 day10 process' day10 process2'
-
--- ----------------------------------------
-
-data Ring a =
-  Ring { _rot  :: Int
-       , _len  :: Int
-       , _vals :: [a]
-       }
-  deriving (Show)
-
-mkRing :: [a] -> Ring a
-mkRing xs = Ring 0 (length xs) xs
-
-rotateRing :: Int -> Ring a -> Ring a
-rotateRing n0 r@(Ring rot len vals)
-  | n == 0    = r
-  | otherwise = Ring ((rot + n) `mod` len) len (ys ++ xs)
-  where
-    n = n0 `mod` len
-    (xs, ys) = splitAt n vals
-
-normalRing :: Ring a -> Ring a
-normalRing r@(Ring rot len v) =
-  rotateRing (len - rot) r
-
-reversePrefix :: Int -> Ring a -> Ring a
-reversePrefix n0 r@(Ring rot len val)
-  | n <= 1 = r
-  | otherwise = Ring rot len $ reverse xs ++ ys
-  where
-    n = n0 `mod` len
-    (xs, ys) = splitAt n val
-
--- ----------------------------------------
---
--- part 1
-
-process' :: String -> String
-process' = show . toRes . shuffle . fromString
-
-toRes :: Ring Int -> Int
-toRes = product . take 2 . _vals
-
-shuffle :: [Int] -> Ring Int
-shuffle = shuffle' 256
-
-shuffle' :: Int -> [Int] -> Ring Int
-shuffle' len =
-    normalRing
-  . foldl' tieKnot (mkRing [0 .. len-1])
-  . zip [0..]
-  where
-    tieKnot :: Ring Int -> (Int, Int) -> Ring Int
-    tieKnot r (step, width) =
-      rotateRing (width + step) $ reversePrefix width r
-
--- ----------------------------------------
-
-fromString :: String -> [Int]
-fromString s = read $ "[" ++ s ++ "]"
-
-ex :: Int
-ex = toRes . shuffle' 5 $ [3, 4, 1, 5]
-
--- result of input day11
-res :: String
-res = process' day10
-
--- test input from adventofcode
-day10 :: String
-day10 = "197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63"
-
--- ----------------------------------------
-
-process2' :: String -> String
-process2' = toRes2 . toDense . _vals . shuffle . prepSeq . fromString2
-
-prepSeq :: [Int] -> [Int]
-prepSeq xs =
-      concat
-    . replicate 64
-    $ (xs ++ [17, 31, 73, 47, 23])
-
-toDense :: [Int] -> [Int]
-toDense = map xors . part 16
-
-xors :: [Int] -> Int
-xors = foldl' xor 0
-
-part :: Int -> [a] -> [[a]]
-part n = unfoldr pick
-  where
-    pick :: [a] -> Maybe ([a], [a])
-    pick [] = Nothing
-    pick xs = Just $ splitAt n xs
-
-toRes2 :: [Int] -> String
-toRes2 = concatMap (printf "%02x")
-
-fromString2 :: String -> [Int]
-fromString2 = map fromEnum
-
--- ----------------------------------------
-
-{-
-The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
-AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
-1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
-1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
--}
+main = main12
+       day10 tieKnots
+       day10 knotHash  -- 2. part
 
 -- ----------------------------------------
--- toys/adventofcode.com/Year17/Day13/Main.hs
+++ toys/adventofcode.com/Year17/Day13/Main.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE TupleSections #-}
 -- solution for
 -- http://adventofcode.com/2017/day/13
 
 module Main where
 
-import Util.Main1    (main1)
+import Util.Main1    (main12)
 import Data.Char     (isDigit, isSpace)
 import Data.List     (foldl')
 
@@ -11,20 +12,27 @@ import Data.List     (foldl')
 
 
 main :: IO ()
-main = main1 day13 process'
+main = main12
+       day13 firewall1
+       day13 firewall2
 
 -- ----------------------------------------
 
-process' :: String -> String
-process' = show . process . fromString
+firewall1 :: String -> String
+firewall1 = show . process . fromString
 
 process :: [(Int, Int)] -> Int
 process =
-  foldl' severe 0 . map (\w -> (w , toSeq $ snd w))
+  foldl' severe 0 . toWall
   where
     severe acc ((i, d), xs)
       | xs !! i == 0 = acc + i * d
       | otherwise    = acc
+
+type Firewall = [((Int, Int), [Int])]
+
+toWall :: [(Int, Int)] -> [((Int, Int), [Int])]
+toWall = map (\w -> (w , toSeq $ snd w))
 
 toSeq :: Int -> [Int]
 toSeq n = cycle xs
@@ -40,10 +48,41 @@ fromString =
   . map (filter (\c -> isDigit c || isSpace c ))
   . lines
 
+-- ----------------------------------------
 
+firewall2 :: String -> String
+firewall2 = show . process2 (10^7) . fromString
+
+-- without mx this might not terminate
+
+process2 :: Int -> [(Int, Int)] -> Int
+process2 mx cf =
+    head
+  . map fst
+  . filter (uncurry isNotCaught)
+  . map (,fw) $ [0..mx]
+  where
+    fw = toWall cf
+
+isNotCaught :: Int -> Firewall -> Bool
+isNotCaught delay =
+  not . foldr caught False
+  where
+    caught :: ((Int, Int), [Int]) -> Bool -> Bool
+    caught ((i, d), xs) acc =
+      xs !! i' == 0 || acc
+      where
+        i' | d <= 2    = (i + delay) `mod` d
+           | otherwise = (i + delay) `mod` (2 * (d-1))
+        -- @!!@ is too slow, but the sequences are periodic
+        -- with length @d + (d-2)@ except for d=1 or d=2
+        -- and delay becomes pretty large, see res2
+
+-- ----------------------------------------
+--
 -- result of input day11
 res :: String
-res = process' day13
+res = firewall1 day13
 
 ex :: String
 ex = "0: 3\n1: 2\n4: 4\n6: 4"
@@ -51,5 +90,9 @@ ex = "0: 3\n1: 2\n4: 4\n6: 4"
 -- test input from adventofcode
 day13 :: String
 day13 = "0: 5\n1: 2\n2: 3\n4: 4\n6: 6\n8: 4\n10: 8\n12: 6\n14: 6\n16: 8\n18: 6\n20: 9\n22: 8\n24: 10\n26: 8\n28: 8\n30: 12\n32: 8\n34: 12\n36: 10\n38: 12\n40: 12\n42: 12\n44: 12\n46: 12\n48: 14\n50: 12\n52: 14\n54: 12\n56: 14\n58: 12\n60: 14\n62: 14\n64: 14\n66: 14\n68: 14\n70: 14\n72: 14\n76: 14\n80: 18\n84: 14\n90: 18\n92: 17\n"
+
+exp2, res2 :: String
+exp2 = "3850260"
+res2 = firewall2 day13
 
 -- ----------------------------------------
