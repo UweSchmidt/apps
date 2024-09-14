659448e3f16526d7a163fb06145426e42675e57a
Uwe Schmidt <uwe.pe.schmidt@gmail.com>
2024-09-14 19:04:03 +0200

    figure811

--- figure.game/app/Main.hs
+++ figure.game/app/Main.hs
@@ -99,12 +99,11 @@ readMoves = do
     then do
       putStr "list of moves : " >> flush
       row <- getLine
-      putStrLn row >> flush
       if null row
         then readMoves
         else case readMaybe row of
           Nothing -> readMoves
-          res -> putStrLn (show res) >> return res
+          res -> return res
     else return Nothing
 
 readInt :: String -> IO Int
